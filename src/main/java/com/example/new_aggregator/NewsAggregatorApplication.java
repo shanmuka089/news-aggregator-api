4b22c4b5e0161a1e6090323712ae3a547f35d3bd
shanmukha <shanmukhashanu5525@gmail.com>
2025-08-16 11:46:05 +0530

    initial repository added

---
+++ src/main/java/com/example/new_aggregator/NewsAggregatorApplication.java
@@ -0,0 +1,14 @@
+package com.example.new_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggregatorApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/new_aggregator/NewsAggregatorApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.new_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggregatorApplicationTests
+{
+
+	@Test
+	void contextLoads() {
+	}
+
+}
